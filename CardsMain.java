0dbc39c4c2e1b478d3936f2c3ae095e5dfafc337
paresh05 <pareshpraveen.surathkal@gmail.com>
2021-09-08 08:18:35 +0530

    Added: Welcome Message

---
+++ CardsMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabzcards;
+
+public class CardsMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the Game of Cards");
+
+	}
+
+}
